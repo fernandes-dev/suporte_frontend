470b8fe9b28e7edcc24873274d2353513debfaaf
cn7polky <eduardo.yugan@gmail.com>
2020-02-20 10:36:50 -0400

    primeiro commit

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <h3>
+      <router-link to="/register">Cadastrar</router-link>
+    </h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Login",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="register">
+    <h1>Cadastro</h1>
+  </div>
+</template>
