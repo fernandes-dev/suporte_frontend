e8d5f8b8e21df824f4478a9d1f7068418782b0b2
cn7polky <eduardo.yugan@gmail.com>
2020-02-20 11:02:48 -0400

    adicionado Vuetify

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="register">
     <h1>Cadastro</h1>
+    <h3>
+      <router-link to="/">Login</router-link>
+      <v-text-field
+      label="Outlined"
+      placeholder="Placeholder"
+      outlined>
+      </v-text-field>
+    </h3>
   </div>
 </template>
